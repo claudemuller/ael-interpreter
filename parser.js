7845695da429e85c46bd331b45a397840ba2c81d
Claude Muller <claude.muller@derivco.co.za>
2017-03-30 10:51:39 +0200

    Interpreter interface

--- parser.js
+++ parser.js
@@ -1,68 +1,92 @@
-var parse = function(tokens) {
-  var parseTree = [];
-  var symbols = {};
-
-  symbol = function(id, nud, lbp, led) {
-    var sym = symbols[id] || {};
-    symbols[id] = {
-      lbp: sym.lbp || lbp,
-      nud: sym.nud || nud,
-      led: sym.led || led
+var parse = function (tokens) {
+  var symbols = {},
+    symbol = function (id, nud, lbp, led) {
+      var sym = symbols[id] || {};
+      symbols[id] = {
+        lbp: sym.lbp || lbp,
+        nud: sym.nud || nud,
+        led: sym.lef || led
+      };
     };
-  };
 
-  var interpretToken = function(token) {
+  var interpretToken = function (token) {
     var sym = Object.create(symbols[token.type]);
     sym.type = token.type;
     sym.value = token.value;
-
     return sym;
   };
 
-  var i =0;
-  var token = function() {
-    return interpretToken(tokens[i]);
-  };
-  var advance = function() {
-    i++;
-    token();
-  };
+  var i = 0, token = function () { return interpretToken(tokens[i]); };
+  var advance = function () { i++; return token(); };
 
-  var expression = function(rbp) {
-    var left,
-      t = token();
-
+  var expression = function (rbp) {
+    var left, t = token();
     advance();
-    if (!t.nud) throw 'Unexpected token: ' + t.type;
+    if (!t.nud) throw "Unexpected token: " + t.type;
     left = t.nud(t);
     while (rbp < token().lbp) {
       t = token();
       advance();
-      if (!t.led) throw 'Unexpected token: ' + t.type;
+      if (!t.led) throw "Unexpected token: " + t.type;
       left = t.led(left);
     }
-
     return left;
-  }
+  };
 
-  var infix = function(id, lbp, rbp, led) {
-    rbp = rbp || ;bp;
-    symbol(id, null, lbp, led || function(left) {
+  var infix = function (id, lbp, rbp, led) {
+    rbp = rbp || lbp;
+    symbol(id, null, lbp, led || function (left) {
       return {
         type: id,
         left: left,
         right: expression(rbp)
       };
     });
-  };
-  var prefix = function(id, rdp) {
-    symbol(id, function() {
+  },
+    prefix = function (id, rbp) {
+      symbol(id, function () {
+        return {
+          type: id,
+          right: expression(rbp)
+        };
+      });
+    };
+
+
+  symbol(",");
+  symbol(")");
+  symbol("(end)");
+
+  symbol("number", function (number) {
+    return number;
+  });
+  symbol("identifier", function (name) {
+    if (token().type === "(") {
+      var args = [];
+      if (tokens[i + 1].type === ")") advance();
+      else {
+        do {
+          advance();
+          args.push(expression(2));
+        } while (token().type === ",");
+        if (token().type !== ")") throw "Expected closing parenthesis ')'";
+      }
+      advance();
       return {
-        type: id,
-        right: expression(rbp)
+        type: "call",
+        args: args,
+        name: name.value
       };
-    });
-  };
+    }
+    return name;
+  });
+
+  symbol("(", function () {
+    value = expression(2);
+    if (token().type !== ")") throw "Expected closing parenthesis ')'";
+    advance();
+    return value;
+  });
 
   prefix("-", 7);
   infix("^", 6, 5);
@@ -72,67 +96,30 @@ var parse = function(tokens) {
   infix("+", 3);
   infix("-", 3);
 
-  symbol(',');
-  symbol(')');
-  symbol('(end)');
+  infix("=", 1, 2, function (left) {
+    (left.type === "call") {
+      (var i = 0; i < left.args.length; i++) {
+        (left.args[i].type !== "identifier") throw "Invalid argument name";
 
-  symbol(')', function() {
-    value = expression(2);
-    if (token().type !== ')') throw 'Expected closing parenthesis ")"';
-    advance();
+        {
+          : "function",
+            : left.name,
+            : left.args,
+            : expression(2)
 
-    return value;
-  });
-  symbol('number', function(number) {
-    return number;
-  });
-  symbol('identifier', function(name) {
-    if (token().type === '(') {
-      var args = [];
-      if (tokens[i + 1].type === ')') advance();
-      else {
-        do {
-          advance();
-          args.push(expression(2));
-        } while (token().type === ',');
-        if (token().type !== ')') throw 'Expected closing parenthesis ")"';
-      }
-      advance();
+          else if (left.type === "identifier") {
+            {
+              : "assign",
+                : left.value,
+                : expression(2)
 
-      return {
-        type: 'call',
-        args: args,
-        name: name.value
-      };
-    }
 
-    return name;
-  });
-  infix('=', 1, 2, function(left) {
-    if (left.type === 'call') {
-      for (var i = 0; i < left.args.lenth; i++) {
-        if (left.args[i].type !== 'identifier') throw 'Invalid argumet name';
-      }
+              throw "Invalid lvalue";
 
-      return {
-        type: 'function',
-        name: left.name,
-        args: left.args,
-        value: expression(2)
-      };
-    } else if (left.tye === 'identifier') {
-      return {
-        type: 'assign',
-        name: left.value,
-        value: expression(2)
-      };
-    } else throw 'Invalid ivalue';
-  });
+              120  	var parseTree = [];
+              (token().type !== "(end)") {
+                .push(expression(0));
 
-  var parseTree = [];
-  while (token().type !== '(end)') {
-    parseTree.push(expression(0));
-  }
+                parseTree;
+              };
 
-  return parseTree;
-}
